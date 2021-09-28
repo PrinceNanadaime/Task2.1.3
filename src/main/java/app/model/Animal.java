1bad2414b82fb29a8c72362170d420dc1b503fd7
Nikita <kotenkov.nikita@mail.ru>
2021-09-28 11:50:47 +0300

    Task 2.1.3. is done

--- src/main/java/app/model/Animal.java
+++ src/main/java/app/model/Animal.java
@@ -1,4 +1,7 @@
 package app.model;
 
 public abstract class Animal {
+    @Override
+    public abstract String toString();
 }
+
--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,31 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
+    private final Timer timer;
+
     @Autowired
-    private Animal animal;
+    public AnimalsCage(@Qualifier("dog") Animal animal, @Qualifier("timer") Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
+
+
+    private final Animal animal;
 
     public void whatAnimalSay() {
-        System.out.println("Say:");
+        System.out.println("\nSay:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
-        System.out.println("________________________");
+        System.out.println(timer.getTime() + "\n");
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
